0d948cc4667fe13e0887255278b8ceb1485b5f81
Antoni Helbich <s33211@pjwstk.edu.pl>
2026-03-12 19:00:07 +0100

    Dodano metodę CalculateAverage do klasy StatisticsHelper

--- apbd-cw1-s33211/apbd-cw1-s33211/StatisticsHelper.cs
+++ apbd-cw1-s33211/apbd-cw1-s33211/StatisticsHelper.cs
@@ -2,5 +2,13 @@
 
 public class StatisticsHelper
 {
-    
+    public static double CalculateAverage(int[] values)
+    {
+        int sum = 0;
+        for(int i = 0; i < values.Length; i++)
+        {
+            sum += values[i];
+        }
+        return sum / values.Length;
+    }
 }
