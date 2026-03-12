82b2b46a6f1a494450540adcedfc57281ed1e963
Antoni Helbich <s33211@pjwstk.edu.pl>
2026-03-12 18:48:02 +0100

    Dodano obsługę danych wejściowych

--- apbd-cw1-s33211/apbd-cw1-s33211/Program.cs
+++ apbd-cw1-s33211/apbd-cw1-s33211/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args) 
+    {
+     Console.WriteLine("Podaj liczby całkowite oddzielone spacją:");
+     string input = Console.ReadLine();
+     string[] parts = input.Split(' ');
+     int[] liczby = new int[parts.Length];
+     for (int i = 0; i < parts.Length; i++)
+     {
+         liczby[i] = int.Parse(parts[i]);
+     }
+     
+     
+    }
+}
+
