11e4f479670c1af738448a3c5df10b8c892818fa
Antoni Helbich <s33211@pjwstk.edu.pl>
2026-03-12 18:54:09 +0100

    Dodano obsługę błędów

--- apbd-cw1-s33211/apbd-cw1-s33211/Program.cs
+++ apbd-cw1-s33211/apbd-cw1-s33211/Program.cs
@@ -2,17 +2,30 @@
 {
     static void Main(string[] args) 
     {
-     Console.WriteLine("Podaj liczby całkowite oddzielone spacją:");
-     string input = Console.ReadLine();
-     string[] parts = input.Split(' ');
-     int[] liczby = new int[parts.Length];
-     for (int i = 0; i < parts.Length; i++)
-     {
-         liczby[i] = int.Parse(parts[i]);
-     }
-     
-     Console.WriteLine("Liczby: " + string.Join(", ", liczby));
-     
+        try
+        {
+            Console.WriteLine("Podaj liczby całkowite oddzielone spacją:");
+            string input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Błąd: Nie podano żadnych liczb.");
+                return;
+            }
+
+            string[] parts = input.Split(' ');
+            int[] liczby = new int[parts.Length];
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                liczby[i] = int.Parse(parts[i]);
+            }
+
+            Console.WriteLine("Liczby: " + string.Join(", ", liczby));
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Błąd: wpisano coś co nie jest liczbą całkowitą.");
+        }
     }
 }
 
